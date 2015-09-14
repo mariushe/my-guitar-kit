cc1b5efa8c02776d51bb8ffac6be999e97c6bfd6
Marius Herring <mariusherring@gmail.com>
2015-09-14 09:11:33 +0200

    Removing deps

--- out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "build_needs_id_847", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
